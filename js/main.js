01562266d9d2d6bc48805b40fd87d11c4fe4c9be
askaper <andrew@skaper.info>
2016-09-05 13:30:34 -0700

    Stylized modals, added screenshots and stock images, added click events

--- js/main.js
+++ js/main.js
@@ -4,10 +4,37 @@ try{Typekit.load();}catch(e){}
 
 $(function() {
 
-	$('.hlp-work').click(function() {
-		console.log('this should be working damnit!')
+	$('.twitter-clone').click(function() {
 		vex.dialog.alert({
-		unsafeMessage: '<h1>This is a test!</h1>'
+			unsafeMessage: '<h2>Twitter Clone</h2>' + '<img class="vex-images" src="img/imac-twitter-clone.png">' +
+				'A working Twitter clone I developed during my time at RockIt Bootcamp putting JavaScript, jQuery, and templating skills (Handlebars.js) into action.' +
+				'<a class="vex-url" href="twitter/index.html">Live demo</a>' +
+				'<a class="vex-url" href="https://github.com/askaper/milestone3/tree/master/twitter"><i class="fa fa-github bottom-icons" aria-hidden="true"></i></a>'
+		})
+	})
+
+	$('.puppy-love').click(function() {
+		vex.dialog.alert({
+			unsafeMessage: '<h2>Puppy Love</h2>' + '<img class="vex-images" src="img/puppy-love-screenshot.png">' +
+			'An exercise in creating a functional page from a static image using symantic HTML and CSS best practices with Chrome Development Tools' +
+			'<a class="vex-url" href="plove/index.html">Live Demo</a>' +
+			'<a class="vex-url" href="https://github.com/askaper/milestone3/tree/master/plove"><i class="fa fa-github bottom-icons" aria-hidden="true"></i></a>'
+		})
+	})
+
+	$('.catalog').click(function() {
+		vex.dialog.alert({
+			unsafeMessage: '<h2>Catalog Redesign</h2>' + '<img class="vex-images" src="img/hlp-catalog.png">' +
+			'A redesign of Halogen Lighting&#39;s 2012 catalog where I made contributions towards design, copy, and photography using Adobe Creative Suite (InDesign, Illustrator, and Photoshop)' +
+			'<a class="vex-url" href="pdf/2012-catalog.pdf"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>'
+		})
+	})
+
+	$('.windrow').click(function() {
+		vex.dialog.alert({
+			unsafeMessage: '<h2>First Website Design</h2>' + '<img class="vex-images" src="img/windrow.png">' +
+			'The first client website I have designed and developed back in 2013 with limited skills. (HTML, CSS, Javascript, PHP, Bootstrap)' +
+			'<a class="vex-url" href="http://www.windrowsolutions.com">Live Demo</a>'
 		})
 	})
 
