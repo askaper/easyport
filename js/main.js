d0dc6efd7473be6b498687276ee76ff1928974d4
askaper <andrew@skaper.info>
2016-09-05 13:40:00 -0700

    Added catalog PDF, changed verbiage, changed links and copy in modals

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(function() {
 			unsafeMessage: '<h2>Twitter Clone</h2>' + '<img class="vex-images" src="img/imac-twitter-clone.png">' +
 				'A working Twitter clone I developed during my time at RockIt Bootcamp putting JavaScript, jQuery, and templating skills (Handlebars.js) into action.' +
 				'<a class="vex-url" href="twitter/index.html">Live demo</a>' +
-				'<a class="vex-url" href="https://github.com/askaper/milestone3/tree/master/twitter"><i class="fa fa-github bottom-icons" aria-hidden="true"></i></a>'
+				'<a class="vex-url" href="https://github.com/askaper/milestone3/tree/master/twitter"><i class="fa fa-github bottom-icons" aria-hidden="true"></i>  View Repo</a>'
 		})
 	})
 
@@ -18,7 +18,7 @@ $(function() {
 			unsafeMessage: '<h2>Puppy Love</h2>' + '<img class="vex-images" src="img/puppy-love-screenshot.png">' +
 			'An exercise in creating a functional page from a static image using symantic HTML and CSS best practices with Chrome Development Tools' +
 			'<a class="vex-url" href="plove/index.html">Live Demo</a>' +
-			'<a class="vex-url" href="https://github.com/askaper/milestone3/tree/master/plove"><i class="fa fa-github bottom-icons" aria-hidden="true"></i></a>'
+			'<a class="vex-url" href="https://github.com/askaper/milestone3/tree/master/plove"><i class="fa fa-github bottom-icons" aria-hidden="true"></i>  View Repo</a>'
 		})
 	})
 
@@ -26,7 +26,7 @@ $(function() {
 		vex.dialog.alert({
 			unsafeMessage: '<h2>Catalog Redesign</h2>' + '<img class="vex-images" src="img/hlp-catalog.png">' +
 			'A redesign of Halogen Lighting&#39;s 2012 catalog where I made contributions towards design, copy, and photography using Adobe Creative Suite (InDesign, Illustrator, and Photoshop)' +
-			'<a class="vex-url" href="pdf/2012-catalog.pdf"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>'
+			'<a class="vex-url" href="2012-catalog.pdf">Click Here to view Catalog  <i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>'
 		})
 	})
 
